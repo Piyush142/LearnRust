a7648964b009a912770195ccec4121b48eabb7df
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-07-06 21:10:05 +0530

    Chapter 5 Notes

---
+++ Chapter5/rectangle/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area1(&rect1)
+    );
+    println!("rect1 is {rect1:#?}");
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+fn area1(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
+}
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
