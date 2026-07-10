2ac02b4937241062b89c9320948075ca60ec5bc4
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-07-10 14:49:47 +0530

    Chapter7 Notes

---
+++ Chapter7/restaurant/src/back_of_house.rs
@@ -0,0 +1,25 @@
+fn fix_incorrect_order() {
+    cook_order();
+    super::deliver_order();
+}
+
+pub enum Appetizer {
+    Soup,
+    Salad,
+}
+
+pub struct Breakfast {
+    pub toast: String,
+    seasonal_fruit: String,
+}
+
+impl Breakfast {
+    pub fn summer(toast: &str) -> Breakfast {
+        Breakfast {
+            toast: String::from(toast),
+            seasonal_fruit: String::from("peaches"),
+        }
+    }
+}
+
+fn cook_order() {}
---
+++ Chapter7/restaurant/src/lib.rs
@@ -0,0 +1,43 @@
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+mod front_of_house;
+
+fn deliver_order() {}
+
+mod back_of_house;
+
+mod customer {
+    use crate::front_of_house::hosting;
+
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn function1() -> Result {
+    return Ok(());
+    // --snip--
+}
+
+fn function2() -> IoResult<()> {
+    return Ok(());
+    // --snip--
+}
