4b881f2878160c23748c33edc21ccaf853857c00
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-07-17 21:44:38 +0530

    Chapter9 Result enum example

--- Chapter9/errorHandling/src/main.rs
+++ Chapter9/errorHandling/src/main.rs
@@ -1,10 +1,19 @@
 use std::fs::File;
+use std::io::{self, Read};
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
+    // let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {error:?}"),
-    };
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Problem opening the file: {error:?}"),
+    // };
+
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let mut username = String::new();
+
+        File::open("hello.txt")?.read_to_string(&mut username)?;
+
+        Ok(username)
+    }
 }
