01889e8aeccc013ebdbaa171fb22210adadc1365
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-08-01 13:47:50 +0530

    Chapter13 Notes: Closures

---
+++ Chapter13/exam13/src/main.rs
@@ -0,0 +1,39 @@
+// use std::thread;
+
+// fn main() {
+//     let list = vec![1, 2, 3];
+//     println!("Before defining closure: {list:?}");
+
+//     thread::spawn(move || println!("From thread: {list:?}"))
+//         .join()
+//         .unwrap();
+// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{list:#?}, sorted in {num_sort_operations} operations");
+}
